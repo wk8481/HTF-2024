b4b1fb21bf9b8fc31ce1fd4837dd236d9f3afb68
Robin Peerlinck <robinpeerlinck@gmail.com>
2024-11-18 17:41:55 +0100

    Added Templates and Scripts

---
+++ installPackages.sh
@@ -0,0 +1,11 @@
+cd src/fn-challenge-1
+npm install
+cd ../fn-challenge-2
+npm install
+cd ../fn-challenge-3
+npm install
+cd ../fn-challenge-4
+npm install
+cd ../fn-challenge-5
+npm install
+cd ../..
